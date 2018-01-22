941679f55c0201297cfc29ef1e2ebc36dd2838d6
LtBackup <jamespratt6@gmail.com>
2018-01-22 13:48:38 -0600

    adds polish and some sound effects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var secretWord;
-var wordBank = ["Metroid", "Mega Man", "Super Mario Bros", "Castlevania", "Contra", "Devil May Cry", "Asteroids", "Galaga", "Donkey Kong", "Mortal Kombat", "Super Smash Bros", "Final Fantasy", "Pac Man", "Fire Emblem", "Legend of Zelda", "Sonic the Hedgehog", "Chrono Trigger", "The Sims", "Halo", "Tomb Raider", "Star Fox", "Pokemon", "Call Of Duty", "Tetris", "Need For Speed", "Grand Theft Auto", "Bomberman", "Prince of Persia", "Doom", "Crash Bandicoot", "Guilty Gear", "Silent Hill", "Half Life", "Metal Gear", "Dance Dance Revolution", "Kingdom Hearts", "Fallout", "Resident Evil", "Bioshock", "Gears of War", "Mass Effect", "God of War", "Diablo", "Starcraft", "Warcraft", "Tekken", "Street Fighter", "Soul Caliber", "Space Invaders"];
+var wordBank = ["Metroid", "Mega Man", "Super Mario Bros", "Castlevania", "Contra", "Devil May Cry", "Asteroids", "Galaga", "Donkey Kong", "Mortal Kombat", "Super Smash Bros", "Final Fantasy", "Pac Man", "Fire Emblem", "Legend of Zelda", "Sonic the Hedgehog", "Chrono Trigger", "The Sims", "Halo", "Tomb Raider", "Star Fox", "Pokemon", "Call Of Duty", "Tetris", "Need For Speed", "Grand Theft Auto", "Bomberman", "Doom", "Crash Bandicoot", "Guilty Gear", "Silent Hill", "Half Life", "Metal Gear", "Dance Dance Revolution", "Kingdom Hearts", "Fallout", "Resident Evil", "Bioshock", "Gears of War", "Mass Effect", "God of War", "Diablo", "Starcraft", "Warcraft", "Tekken", "Street Fighter", "SoulCaliber", "Space Invaders", "Medal of Honor", "Paperboy", "Doom", "Guitar Hero", "Angry Birds", "Gran Turismo", "Pong", "Portal", "Braid", "Mario Kart", "Mario Party", "Wolfenstein", "Myst", "Quake", "Counter Strike", "SimCity", "Civilization", "Minecraft", "The Oregon Trail", "Doodle Jump", "Advance Wars", "Breath of Fire", "Defender", "Joust", "Gauntlet", "Double Dragon", "Prince of Persia", "Shadow of the Colossus", "Streets of Rage", "The Secret of Monkey Island", "Grim Fandango", "Lemmings", "Adventure", "Gunstar Heroes", "Secret of Mana", "Earthbound", "Panzer Dragoon", "Thief", "Shenmue", "Unreal", "Deus Ex", "Jet Set Radio", "Max Payne", "Burnout", "Phoenix Wright", "Katamari Damacy", "The Elder Scrolls", "Okami", "Dead Space", "Uncharted", "Rock Band", "Dark Souls", "The Witcher", "Snake", "Centipede", "Zero Wing", "Missile Command", "Missile Command"];
 var guessedLetter;
-var lettersGuessed = [" "];
+var lettersGuessed = [];
 var filledLetters = [];
-var lives = 4;
+var lives = 3;
 var wins = 0;
 var losses = 0;
 
@@ -14,6 +14,11 @@ var livesDisplay = document.querySelector("#lives");
 var winsDisplay = document.querySelector("#wins");
 var lossesDisplay = document.querySelector("#losses");
 var guessedLettersDisplay = document.querySelector("#guessedLetters");
+
+var audio1Up = new Audio("../audio/smb_1-up.wav");
+var audioWrong = new Audio("../audio/bosspain.wav");
+//var audioWin = new Audio("../audio/");
+//var audioLose = new Audio("../audio/");
 
 function begin(event) {
     document.removeEventListener("keyup", begin);
@@ -105,7 +110,7 @@ function alreadyGuessed(letter) {
 function reset() {
     lettersGuessed = [];
     filledLetters = [];
-    lives = 4;
+    lives = 3;
     document.removeEventListener("keyup", game);
     document.addEventListener("keyup", begin);
 }
@@ -120,6 +125,7 @@ function game(event) {
                 fillAnswer(guessedLetter);
                 updateAnswer();
                 lives++;
+                audio1Up.play();
                 updateLives();
 
                 if (filledLetters.every(isCorrect)) {
@@ -133,6 +139,7 @@ function game(event) {
             }
             else {
                 lives--;
+                audioWrong.play();
                 updateLives();
                 console.log(typeof lives);
                 if (lives === 0) {
@@ -150,10 +157,12 @@ function game(event) {
         }
         else {
             statusDisplay.innerText = "You already guessed that letter!";
+            audioWrong.play();
         }
     }
     else {
         statusDisplay.innerText = "Enter a letter, please";
+        audioWrong.play();
     }
 }
 
